ec7e225f0b5e563cda00539015a109806f6d6a91
Don Undeen <donundeen@gmail.com>
2024-11-12 15:01:53 -0500

    new notes

--- system/installservice.sh
+++ system/installservice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp ~/pikioskwebpage/system/units/pikioskwebpage.service /lib/systemd/user/
+cp /home/pi/pikioskwebpage/system/units/pikioskwebpage.service /lib/systemd/user/
 systemctl daemon-reload
 systemctl --user enable pikioskwebpage.service 
