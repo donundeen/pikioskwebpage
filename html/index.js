e05aa81b78d4276e21dfa90d53ba27c58ba757ae
Don Undeen <donundeen@gmail.com>
2024-11-12 14:06:19 -0500

    new notes

--- html/index.js
+++ html/index.js
@@ -65,31 +65,37 @@ $(function() {
         // try parsing a json object into a series of javascript function calls. maybe it will work?
         if(msg.address == "/screen/p5json"){
             let p5json = msg.data.p5json;
-            let farray=[];
             for(let i = 0 ; i< p5json.length; i++){
+                let farray=[];                
+                let fstring = false;
                 let func = p5json[i];
-                let fname = func.f;
-                let argarray = [];
-                for(let j = 0; j < func.args.length; j++){
-                    let arg = func.args[j];
-                    if(typeof arg === "String"){
-                        argarray.push( "'"+arg+"'");
-                    }else{
-                        argarray.push(arg);
+                if(func.t){
+                    fstring = func.t;
+                }else{
+                    let fname = func.f;
+                    let argarray = [];
+                    for(let j = 0; j < func.args.length; j++){
+                        let arg = func.args[j];
+                        console.log(typeof arg);
+                        if(typeof arg === "string"){
+                            argarray.push( "'"+arg+"'");
+                        }else{
+                            argarray.push(arg);
+                        }
                     }
+                    let argstring = argarray.join(", ");
+                    fstring = fname+"("+argstring+");"
                 }
-                let argstring = argarry.join(", ");
-                fstring = fname+"("+argstring+");"
-                farray.push(fstring);
+                try{
+                    console.log("eval", fstring);
+                    eval(fstring);
+                }catch(e){
+                    console.log("p5json error", e);
+                    let msg = {msg : e.message, fstring: fstring, func: func, p5json : p5json, address : "p5error"};
+                    message("toadminmessage", msg);
+                }            
+    
             }
-            let p5code = farray.join("\n");
-            try{
-                eval(p5code);
-            }catch(e){
-                console.log("p5json error", e);
-                let msg = {msg : e.message, p5code: p5code, p5json : p5json, address : "p5error"};
-                message("toadminmessage", msg);
-            }            
         }
 
     }
@@ -120,14 +126,14 @@ function setup() {
     // Create screen reader accessible description
     textOutput();
   
-    createCanvas(720, 400);
+    createCanvas(screen.width, screen.height);
   
     // Use degrees as units for angles
     // The arc() function uses angles
     angleMode(DEGREES);
   
     // Draw a light gray background
-    background(220);
+    background(240);
   
   
   }
